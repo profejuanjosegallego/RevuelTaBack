1eedff4558f76c3306b5ee618d0f4db59ff7c291
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2026-06-18 11:12:30 -0500

    ok

---
+++ src/main/java/com/example/ReVueltaBack/ReVueltaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReVueltaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReVueltaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReVueltaBackApplication.class, args);
+	}
+
+}
