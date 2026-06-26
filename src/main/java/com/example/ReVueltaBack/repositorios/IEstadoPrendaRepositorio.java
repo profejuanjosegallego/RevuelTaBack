a3b3621cf2b79ea2441793bea7324d9be9a6c223
pitorro123 <pipe.kmx@hotmail.com>
2026-06-25 20:14:10 -0500

    fix: corregir ortografía del repositorio a español y añadir interfaz

--- src/main/java/com/example/ReVueltaBack/repositorios/IEstadoPrendaRepositorio.java
+++ src/main/java/com/example/ReVueltaBack/repositorios/IEstadoPrendaRepositorio.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.UUID;
 
-public interface EstadoPrendaRepository extends JpaRepository<EstadoPrenda, UUID> {
+public interface IEstadoPrendaRepositorio extends JpaRepository<EstadoPrenda, UUID> {
 
     List<EstadoPrenda> findByNombreContainingIgnoreCase(String nombre);
 
