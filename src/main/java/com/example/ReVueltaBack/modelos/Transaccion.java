53f61a6ea3e124157567e96c7682f8eef0048eac
Andres Villa <pipevilla11@gmail.com>
2026-07-01 19:21:51 -0500

    crear entidad transaccion con su atributos y getters/setters

---
+++ src/main/java/com/example/ReVueltaBack/modelos/Transaccion.java
@@ -0,0 +1,84 @@
+package com.example.ReVueltaBack.modelos;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "transacciones")
+public class Transaccion {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column (name = "tipo", nullable = false, unique = true, length = 50)
+    private String tipo;
+
+    @Column (name = "monto", nullable = false, unique = true, length = 50)
+    private Double monto;
+
+    @Column (name = "estado", nullable = false, unique = true, length = 50)
+    private String estado;
+
+    @Column (name = "referencia_pago", nullable = false, unique = true, length = 30)
+    private String referencia_pago;
+
+    @Column (name = "fecha", nullable = false, unique = true, length = 10)
+    private LocalDate fecha;
+
+    @Column ()
+    private String comprobante;
+
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public Double getMonto() {
+        return monto;
+    }
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getReferencia_pago() {
+        return referencia_pago;
+    }
+    public void setReferencia_pago(String referencia_pago) {
+        this.referencia_pago = referencia_pago;
+    }
+    public LocalDate getFecha() {
+        return fecha;
+    }
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+    public String getComprobante() {
+        return comprobante;
+    }
+    public void setComprobante(String comprobante) {
+        this.comprobante = comprobante;
+    }
+
+
+
+}
