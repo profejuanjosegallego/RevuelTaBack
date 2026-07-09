1ce8e14be5ca77ac0c6d2ea3e299bfbad5d841b2
stivengilperez-source <stivengilperez@gmail.com>
2026-07-09 10:22:37 -0500

    categoria repository

---
+++ src/main/java/com/example/ReVueltaBack/repositorios/ICategoriaRepositorio.java
@@ -0,0 +1,8 @@
+package com.example.ReVueltaBack.repositorios;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICategoriaRepositorio extends jpaRepository<Catego {
+
+}
