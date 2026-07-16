a6417838d4268b42f5edfcc49cced0fbff9968b6
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2026-07-16 11:21:43 -0500

    feat se crea validacion usuario

---
+++ src/main/java/com/example/ReVueltaBack/validaciones/usuario/IValidacionUsuario.java
@@ -0,0 +1,19 @@
+package com.example.ReVueltaBack.validaciones.usuario;
+
+import com.example.ReVueltaBack.modelos.Usuario;
+
+public interface IValidacionUsuario {
+
+    //obligatoriedad en el nombre de usuario
+    void validarNombreObligatorio(String nombre);
+
+    //Formato del correo
+    void validarCorreoFormato(String correo);
+
+    //tamaño del hash contraseña
+    void validarContraseñaLongitud(String contraseñaHash);
+
+    //funcion para unificar las validaciones
+    void validarUsuario(Usuario usuario);
+
+}
