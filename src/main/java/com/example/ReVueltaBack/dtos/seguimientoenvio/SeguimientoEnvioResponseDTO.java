1993347d7df7e0fdd9e8673760c0b10f637e4c78
AndresAcosta01 <andresacost12a@gmail.com>
2026-07-27 12:10:52 -0500

    Refactor: Se completa lógica del DTO response de seguimientoEnvio

--- src/main/java/com/example/ReVueltaBack/dtos/seguimientoenvio/SeguimientoEnvioResponseDTO.java
+++ src/main/java/com/example/ReVueltaBack/dtos/seguimientoenvio/SeguimientoEnvioResponseDTO.java
@@ -1,9 +1,13 @@
 package com.example.ReVueltaBack.dtos.seguimientoenvio;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.example.ReVueltaBack.modelos.SeguimientoEnvio;
 
 public record SeguimientoEnvioResponseDTO(
 
+        UUID id,
         String estado,
         String descripcion,
         String ubicacion,
@@ -12,5 +16,19 @@ public record SeguimientoEnvioResponseDTO(
         Double longitud
 
 ) {
+
+    public SeguimientoEnvioResponseDTO fromEntity(SeguimientoEnvio seguimientoEnvio){
+        
+        return new SeguimientoEnvioResponseDTO(
+            seguimientoEnvio.getId(),
+            seguimientoEnvio.getEstado(),
+            seguimientoEnvio.getDescripcion(),
+            seguimientoEnvio.getUbicacion(),
+            seguimientoEnvio.getFecha_hora(),
+            seguimientoEnvio.getLatitud(),
+            seguimientoEnvio.getLongitud()
+        );
+
+    }
 
 }
