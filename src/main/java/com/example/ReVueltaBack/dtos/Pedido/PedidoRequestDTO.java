2f01288c10bdefd684ffaa6d791d469893fa5843
alexispernilla <alexispernilla@hotmail.com>
2026-07-27 11:27:08 -0500

    feat se agrega toentity() al dto de request usuario

--- src/main/java/com/example/ReVueltaBack/dtos/Pedido/PedidoRequestDTO.java
+++ src/main/java/com/example/ReVueltaBack/dtos/Pedido/PedidoRequestDTO.java
@@ -13,5 +13,6 @@ public record PedidoRequestDTO(
     UUID idComprador
     
 ) {
+    
 
 }
--- src/main/java/com/example/ReVueltaBack/dtos/usuario/UsuarioRequestDTO.java
+++ src/main/java/com/example/ReVueltaBack/dtos/usuario/UsuarioRequestDTO.java
@@ -1,5 +1,10 @@
 package com.example.ReVueltaBack.dtos.Usuario;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.example.ReVueltaBack.modelos.Usuario;
+
 public record UsuarioRequestDTO( 
     
     String nombre,
@@ -7,4 +12,19 @@ public record UsuarioRequestDTO(
     String contraseña,
     String rol
 
-) {}
+) {
+
+    public Usuario toEntity(){
+
+        Usuario usuario= new Usuario();
+        usuario.setNombre(nombre);
+        usuario.setCorreo(correo);
+        usuario.setRol(rol);
+        usuario.setActivo(true);
+        usuario.setColor_avatar("#2563EB");
+        usuario.setFecha_registro(LocalDateTime.now());
+        return usuario;
+
+
+    }
+}
