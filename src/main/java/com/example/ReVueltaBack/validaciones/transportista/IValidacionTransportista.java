9d3d833a0e6b09f5d1f105cd6781ffd9b3fc32e2
Hawasly Code <hawaslycode@gmail.com>
2026-07-28 04:07:12 -0500

    Validaciones transportistas

---
+++ src/main/java/com/example/ReVueltaBack/validaciones/transportista/IValidacionTransportista.java
@@ -0,0 +1,11 @@
+package com.example.ReVueltaBack.validaciones.transportista;
+
+import com.example.ReVueltaBack.modelos.Transportista;
+
+public interface IValidacionTransportista {
+
+    void validarNombreObligatorio(String nombre);
+    void validarZonaCoberturaLongitud(String zonaCobertura);
+    void validarPlacaFormato(String placa);
+    void validarTransportista(Transportista transportista);
+}
