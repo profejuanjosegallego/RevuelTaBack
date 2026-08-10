911e58f2ef35fa5990c3e9abb445c4d0bdde18be
AndresAcosta01 <andresacost12a@gmail.com>
2026-08-10 00:45:07 -0500

    Feat: Se crea el controlador REST de : endpoints CRUD en

---
+++ src/main/java/com/example/ReVueltaBack/controladores/SeguimientoEnvioControlador.java
@@ -0,0 +1,56 @@
+package com.example.ReVueltaBack.controladores;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ReVueltaBack.dtos.seguimientoenvio.SeguimientoEnvioRequestDTO;
+import com.example.ReVueltaBack.dtos.seguimientoenvio.SeguimientoEnvioResponseDTO;
+import com.example.ReVueltaBack.servicios.seguimientoenvio.ISeguimientoEnvioServicio;
+
+@RestController
+@RequestMapping("/api/seguimiento-envio")
+public class SeguimientoEnvioControlador {
+
+    private final ISeguimientoEnvioServicio servicioSeguimientoEnvio;
+
+    public SeguimientoEnvioControlador(ISeguimientoEnvioServicio servicioSeguimientoEnvio) {
+        this.servicioSeguimientoEnvio = servicioSeguimientoEnvio;
+    }
+
+    @PostMapping
+    public ResponseEntity<SeguimientoEnvioResponseDTO> crear(@RequestBody SeguimientoEnvioRequestDTO dto) {
+        SeguimientoEnvioResponseDTO seguimientoCreado = servicioSeguimientoEnvio.crear(dto);
+        return ResponseEntity.created(null).body(seguimientoCreado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SeguimientoEnvioResponseDTO>> listar() {
+        return ResponseEntity.ok(servicioSeguimientoEnvio.listar());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SeguimientoEnvioResponseDTO> buscarPorId(@PathVariable UUID id) {
+        return ResponseEntity.ok(servicioSeguimientoEnvio.buscarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SeguimientoEnvioResponseDTO> actualizar(@PathVariable UUID id, @RequestBody SeguimientoEnvioRequestDTO dto) {
+        return ResponseEntity.ok(servicioSeguimientoEnvio.actualizar(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<SeguimientoEnvioResponseDTO>eliminar(@PathVariable UUID id){
+        servicioSeguimientoEnvio.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
