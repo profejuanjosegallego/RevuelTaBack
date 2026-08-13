cd5796a1fcc5e5e58224ee1383d283720b54bd74
stivengilperez-source <stivengilperez@gmail.com>
2026-08-13 10:30:20 -0500

    ok steven

---
+++ src/main/java/com/example/ReVueltaBack/dtos/Categoria/CategoriaRequestDTO.java
@@ -0,0 +1,25 @@
+package com.example.ReVueltaBack.dtos.Categoria;
+
+import com.example.ReVueltaBack.modelos.Categoria;
+
+public record CategoriaRequestDTO(
+    String nombre,
+    String descripcion,
+    String slug,
+    String icono,
+    Boolean activa,
+    Integer orden
+) {
+    public Categoria toEntity(){
+        Categoria categoria=new Categoria();
+        categoria.setNombre(this.nombre);
+        categoria.setDescripcion(this.descripcion);
+        categoria.setSlug(this.slug);
+        categoria.setIcono(this.icono);
+        categoria.setActiva(this.activa);
+        categoria.setOrden(this.orden);
+        return categoria;
+        
+    }
+
+}
---
+++ src/main/java/com/example/ReVueltaBack/dtos/Categoria/CategoriaResponseDTO.java
@@ -0,0 +1,30 @@
+package com.example.ReVueltaBack.dtos.Categoria;
+
+import java.util.UUID;
+
+import com.example.ReVueltaBack.modelos.Categoria;
+
+public record CategoriaResponseDTO(
+
+        UUID id,
+        String nombre,
+        String descripcion,
+        String slug,
+        String icono,
+        Boolean activa,
+        Integer orden
+){
+    public static CategoriaResponseDTO fromEntity(Categoria Categoria) {
+
+        return new CategoriaResponseDTO(
+                Categoria.getId(),
+                Categoria.getNombre(),
+                Categoria.getDescripcion(),
+                Categoria.getSlug(),
+                Categoria.getIcono(),
+                Categoria.getActiva(),
+                Categoria.getOrden()
+
+        );
+    }
+}
