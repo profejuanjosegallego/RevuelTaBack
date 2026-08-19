3c0dea23fb658e5a816b862ea80346491de760d0
AndresAcosta01 <andresacost12a@gmail.com>
2026-08-19 07:57:36 -0500

    fix: agrega idEnvio a DTO de seguimiento envio

--- src/main/java/com/example/ReVueltaBack/dtos/seguimientoenvio/SeguimientoEnvioRequestDTO.java
+++ src/main/java/com/example/ReVueltaBack/dtos/seguimientoenvio/SeguimientoEnvioRequestDTO.java
@@ -1,7 +1,9 @@
 package com.example.ReVueltaBack.dtos.seguimientoenvio;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+import com.example.ReVueltaBack.modelos.Envio;
 import com.example.ReVueltaBack.modelos.SeguimientoEnvio;
 
 public record SeguimientoEnvioRequestDTO(
@@ -11,20 +13,23 @@ public record SeguimientoEnvioRequestDTO(
         String ubicacion,
         LocalDateTime fechaHora,
         Double latitud,
-        Double longitud
+        Double longitud,
+        UUID idEnvio
 
 ) {
 
     public SeguimientoEnvio toEntity() {
 
         SeguimientoEnvio seguimientoEnvio = new SeguimientoEnvio();
+        Envio envio = new Envio();
+        envio.setId(idEnvio);
         seguimientoEnvio.setEstado(estado);
         seguimientoEnvio.setDescripcion(descripcion);
         seguimientoEnvio.setUbicacion(ubicacion);
         seguimientoEnvio.setFecha_hora(fechaHora);
         seguimientoEnvio.setLatitud(latitud);
         seguimientoEnvio.setLongitud(longitud);
+        seguimientoEnvio.setEnvio(envio);
         return seguimientoEnvio;
-
     }
 }
